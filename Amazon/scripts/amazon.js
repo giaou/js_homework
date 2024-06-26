13d834c68ca9b1d1fea509732ffd6998f4be69dd
Giao U <lesliegiao.u@gmail.com>
2024-06-26 10:31:00 -0500

    Using selector to choose the item quantity

--- Amazon/scripts/amazon.js
+++ Amazon/scripts/amazon.js
@@ -27,7 +27,7 @@ products.forEach((product) => {
         )}</div>
 
         <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
                 <option selected value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -59,6 +59,7 @@ document.querySelector(".js-products-grid").innerHTML = prodcutsHtml;
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
     const productId = button.dataset.productId;
+    console.log(productId);
 
     let matchingItem;
 
@@ -68,12 +69,17 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
       }
     });
 
+    const itemQuantity = document.querySelector(
+      `.js-quantity-selector-${productId}`
+    ).value;
+    console.log(itemQuantity);
+
     if (matchingItem) {
       matchingItem.quantity += 1;
     } else {
       cart.push({
         productId: productId,
-        quantity: 1,
+        quantity: Number(itemQuantity),
       });
     }
 
